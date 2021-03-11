0507023464a64a30aab875321e4395821f466720
Joro_Kudo <zfessj@bbcag.ch>
2021-03-11 14:43:17 +0100

    added structure

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repository;
+
+
+use App\Database\ConnectionHandler;
+use Exception;
+class UserRepository extends Repository
+{
+    protected $tableName = 'user';
+    public function create($firstName, $lastName, $email, $password)
+    {
+        $query = "INSERT Into {$this->tableName} (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password');";
+
+        $connection = ConnectionHandler::getConnection();
+
+        $statement = $connection->prepare($query);
+
+
+        if ($statement == false){
+            throw new Exception($connection->error);
+        }
+
+        if (!$statement->execute()) {
+            throw new Exception($statement->error);
+        }
+    }
+
+}
