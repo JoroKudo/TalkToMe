961ce7cc85e97478fe5a7765df0c1cd9129996c7
Joro_Kudo <zfessj@bbcag.ch>
2021-03-12 08:52:00 +0100

    added create new user

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ return array(
     'database' => array(
         'host'     => 'localhost',
         'username' => 'root',
-        'password' => '',
+        'password' => '1234',
         'database' => 'mvcdemo',
     ),
 
