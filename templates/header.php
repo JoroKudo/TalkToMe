62cd296928dbe298670eeb8c690917c2917b1f67
Joro_Kudo <zfessj@bbcag.ch>
2021-03-12 08:54:22 +0100

    addes signup

--- templates/header.php
+++ templates/header.php
@@ -33,6 +33,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/user/login">Login</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/user/create">Sign>Up</a>
+                </li>
             </ul>
         </div>
     </nav>
