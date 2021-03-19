47d050af670fbeb8186c8dc631ff6de997b1e114
Joro_Kudo <zfessj@bbcag.ch>
2021-03-19 10:43:32 +0100

    added working chat (chat works but is not final)

--- templates/chat/index.php
+++ templates/chat/index.php
@@ -1,20 +1,19 @@
-
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
 
 <!------ Include the above in your HEAD tag ---------->
-
 
 
 <head>
     <link href='https://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
     <link href="/css/chat-style.css" rel="stylesheet">
 
-    </head>
-<div  ng-cloak ng-app="chatApp">
-    <div class='chatbox' ng-controller="MessageCtrl as chatMessage">
+</head>
+<div ng-cloak ng-app="chatApp">
+    <body class='chatbox' ng-controller="MessageCtrl as chatMessage" >
         <div class='chatbox__user-list'>
+
             <h1>Friends Online</h1>
             <div class='chatbox__user--active'>
                 <p>Maelyn Pepe</p>
@@ -34,31 +33,47 @@
         </div>
 
 
-                <?php if (empty($mgss)): ?>
-                    <div class="dhd">
-                        <p class="item message">no messages yet.</p>
-                    </div>
-                <?php else: ?>
-                    <?php foreach ($mgss as $msg): ?>
-                          <div class="chatbox__messages" ng-repeat="message in messages">
-                            <div class="chatbox__messages__user-message">
-                                <div class="chatbox__messages__user-message--ind-message">
-                                    <p class="name">{{message.Name}}</p>
-                                    <br/>
-                                    <div class="message"><?= $msg->message; ?></div>
-                                </div>
-                            </div>
-                        </div>
+        <script type="text/javascript">
+            function toBottom()
+            {
+                window.scrollTo(5, document.body.scrollHeight);
+            }
+            window.onload=toBottom;</script>
 
-                    <?php endforeach; ?>
-                <?php endif; ?>
+        <?php if (empty($mgss)): ?>
+            <div class="dhd">
+                <p class="item message">no messages yet.</p>
+            </div>
+        <?php else: ?>
+            <?php foreach ($mgss as $msg): ?>
+
+                <div class="chatbox__messages" ng-repeat="message in messages">
+                    <div class="chatbox__messages__user-message">
+                        <div class="chatbox__messages__user-message--ind-message">
+
+                            <p class="name"><?= $user->firstname; ?></p>
+                            <br/>
+                            <div class="message"><?= $msg->message; ?></div>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach; ?>
+        <?php endif; ?>
 
         <div class="row">
-            <form action="/chat/doCreate" method="post" >
+            <form action="/chat/doCreate" method="post">
                 <div>
 
-                    <input id="msg" name="msg" type="text" class="form-control">
-                    <button type="submit" name="sennd" class="btn btn-primary">Absenden</button>
+
+                    <input id="msg" name="msg" type="text" class="form-control formobj">
+
+                    <button type="submit" name="sennd" class="btn btn-primary formobj">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                             class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                            <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                        </svg>
+                    </button>
                 </div>
 
 
@@ -66,6 +81,7 @@
         </div>
 
 
-    </div>
+    </body>
+
 
 
