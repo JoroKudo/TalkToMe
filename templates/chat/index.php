534825bf7543cd1e1e39601f1ca4e74e47c62fe4
zhatzi <zhatzi@bbcag.ch>
2021-03-25 14:13:25 +0100

    when submit butten gets clickt, page scrolls down

--- templates/chat/index.php
+++ templates/chat/index.php
@@ -12,77 +12,73 @@
     <div id="chatbox" class='chatbox' ng-controller="MessageCtrl as chatMessage">
 
 
+        <div id="chatContent"></div>
+        <div class="row">
 
-    <div id="chatContent"></div>
-    <div class="row">
+            <input id="msgText" name="msgText" type="text" value="" class="guiobj">
+            <button id="button" onclick="scrollSend()" name="sennd" type="submit" class="btn btn-primary guiobj">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                     class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                    <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                </svg>
+            </button>
 
-        <input id="msgText" name="msgText" type="text" value="" class="guiobj">
-        <button onclick="scrollSend()" name="sennd" type="submit" class="btn btn-primary guiobj">
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
-                 class="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-            </svg>
-        </button>
+        </div>
 
-    </div>
-
-    <script>
-        var executed = false;
-        setInterval(updateChat, 2000);
-
+        <script>
+            var executed = false;
+            setInterval(updateChat, 2000);
 
 
-        function updateChat() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    var chatContentDiv = document.getElementById("chatContent");
-                    chatContentDiv.innerHTML = this.responseText;
-                    if (!executed) {
-                        executed = true;
-                        scrolldown();
+            function updateChat() {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var chatContentDiv = document.getElementById("chatContent");
+                        chatContentDiv.innerHTML = this.responseText;
+                        if (!executed) {
+                            executed = true;
+                            scrolldown();
 
+                        }
                     }
+                };
+                xhttp.open("POST", "chat/load", true);
+                xhttp.send();
+            }
+
+            function scrollSend() {
+                sendChatText();
+                updateChat();
+                inputDelete();
+                scrolldown()
+
+            }
+
+            function scrolldown() {
+
+                var chatboxDiv = document.getElementById("chatbox");
+                chatboxDiv.scrollTo(0, chatboxDiv.scrollHeight)
+            }
+
+            function sendChatText() {
+                let msgTextBox = document.getElementById("msgText");
+                var msgText = msgTextBox.value
+                var data = new FormData();
+                data.append('message', msgText);
+                let xhttp = new XMLHttpRequest();
+                xhttp.open("POST", "/chat/doCreate", true);
+                xhttp.send(data);
+                updateChat();
 
 
+            }
 
-                }
-            };
-            xhttp.open("POST", "chat/load", true);
-            xhttp.send();
+            function inputDelete() {
+                document.getElementById("msgText").value = '';
+            }
 
 
-        }
-
-        function scrollSend(){
-            sendChatText();
-            updateChat();
-            inputDelete();
-
-        }
-
-        function scrolldown(){
-
-            var chatboxDiv = document.getElementById("chatbox");
-            chatboxDiv.scrollTo(0, chatboxDiv.scrollHeight)
-        }
-
-        function sendChatText() {
-            let msgTextBox = document.getElementById("msgText");
-            var msgText = msgTextBox.value
-            var data = new FormData();
-            data.append('message', msgText);
-            let xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "/chat/doCreate",  true);
-            xhttp.send(data);
-            updateChat();
-        }
-
-        function inputDelete(){
-            document.getElementById("msgText").value = '';
-
-        }
-
-    </script>
+        </script>
 
 
